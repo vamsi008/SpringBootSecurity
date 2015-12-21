1b4b9289d2a9f594086addc5ac4a3014a9379708
vamsi008 <vamsee.chava@gmail.com>
2015-12-21 12:15:55 +0530

    Refactored the code .

--- src/main/java/com/employee/app/service/user/UserServiceImpl.java
+++ src/main/java/com/employee/app/service/user/UserServiceImpl.java
@@ -1,16 +1,11 @@
 package com.employee.app.service.user;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.employee.app.model.auth.Role;
 import com.employee.app.model.auth.User;
-import com.employee.app.model.auth.UserRole;
 import com.employee.app.repository.UserRepository;
 
 @Service
@@ -38,18 +33,6 @@ public class UserServiceImpl implements UserService {
 		return userRepository.findAll();
 	}
 
-	@Override
-	public User create(UserCreateForm form) {
-		User user = new User();
-		user.setEmailAddress(form.getEmail());
-		user.setPassword(new BCryptPasswordEncoder().encode(form.getPassword()));
-		Role role = form.getRole();
 
-		List<UserRole> roleList = new ArrayList<UserRole>();
-		UserRole userRole = new UserRole();
-		userRole.setRole(role);
-		user.setUserRoles(roleList);
-		return userRepository.save(user);
-	}
 
 }
